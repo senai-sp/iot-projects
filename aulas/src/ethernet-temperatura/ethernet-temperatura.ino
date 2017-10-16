c1853fba0767c490783128d2687b5c841446f1a7
Paolo Bueno <paolohaji@gmail.com>
2017-10-16 21:54:46 +0000

    debug

--- aulas/src/ethernet-temperatura/ethernet-temperatura.ino
+++ aulas/src/ethernet-temperatura/ethernet-temperatura.ino
@@ -19,13 +19,19 @@ const int LM35_PIN = A1;
 
 int state = STATE_REQUEST;
 
+int readStarted = 0;
+
 // Endereço MAC para nossa placa de rede Ethernet
 // Em sala, substituia o último valor pelo nº do seu Kit
 // para garantir que não haja MACs duplicados
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x42 };
 
-// Ip estático para este arduino caso o DHCP falhe
-IPAddress ip(192, 168, 0, 177);
+// IP estático para este arduino caso o DHCP falhe
+IPAddress ip(192, 168, 3, 177);
+
+// IP do servidor
+// TODO: mudar para valor real
+IPAddress server(192, 168, 3, 67);
 
 EthernetClient client;
 
@@ -42,24 +48,29 @@ void setup() {
     Ethernet.begin(mac, ip);
     Serial.println("Fixed IP:" + String(Ethernet.localIP()));
   } else {
-    Serial.println("IP address granted:" + String(Ethernet.localIP()));
+    Serial.print("IP address granted: ");
+    Serial.println(Ethernet.localIP());
   }
   // Esperar 1s para término da inicialização da placa Ethernet
   delay(1000);
 }
 
 void loop() {
+  // Ler a temperatura
+  int temperatureRead;
+  float temperatureCelcius;
   // Estrutura de decisão com possibilidade de vários caminhos em cima de um único valor
   // também pode ser implementado através de `if`s
   // https://www.arduino.cc/en/Reference/SwitchCase
   switch(state) {
     case STATE_REQUEST:
-      // Ler a temperatura
-      int temperatureRead = analogRead(LM35_PIN);
-      float temperatureCelcius = map(leitura, 0, 1023, 0, 5000) / 10.0;
-      
+      temperatureRead = analogRead(LM35_PIN);
+      temperatureCelcius = map(temperatureRead, 0, 1023, 0, 5000) / 10.0;
       Serial.println("Connecting to server...");
-      if (client.connect(server, 80)) {
+      if (client.connect(server, 8080)) {
+        // Montar os dados a serem enviados
+        String json = "{\"temp\":" + String(temperatureCelcius) + ",\"id\":42}";
+        
         Serial.println("Connection successful");
         
         // Construindo a mensagem HTTP POST
@@ -69,10 +80,10 @@ void loop() {
         
         // O cabeçalho Host especifica o 'nome' do servidor que queremos acessar
         // Este valor deve ser alterado para o valor do IP do servidor
-        client.println("Host: 127.0.0.1");
+        client.print("Host: "); client.print(server); client.println(":8080");
     
         // O cabeçalho Content-Type especifica o tipo de conteúdo de nossa mensagem,
-        // neste caso é uma String no formato JSON
+        // neste caso é um texto no formato JSON
         // outros valores possíveis seriam:
         // text/plain: texto qualquer
         // text/html: página HTML
@@ -80,6 +91,10 @@ void loop() {
         client.println("Content-Type: application/json");
         // esses valores são chamados de mime types:
         // https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Basico_sobre_HTTP/MIME_types
+
+        // Content-Length deve conter o tamanho do conteúdo da mensagem em bytes,
+        // por sorte cada caracter ASCII tem 1 byte, então podemos utilizar String::length()
+        client.print("Content-Length: "); client.println(json.length());
     
         // Este cabeçalho especifica que o servidor deve terminar a conexão após a troca de mensagens
         client.println("Connection: close");
@@ -87,30 +102,37 @@ void loop() {
         // Esta linha em branco separa os headers do conteúdo da mensagem HTTP,
         // sem ela o formato está inválido
         client.println();
-    
-        client.println("{\"temp\":" + String(temperatureCelcius) + ",\"id\":42}")
+
+        client.println(json);
     
         // O método flush() força o envio de todo o texto acima através da placa Ethernet
         // caso não o utilizemos, é possível que nossa mensagem fique 'presa' na memória da própria placa
         // e não seja enviada
         client.flush();
         
-        // Mudamos para o estado de esperar uma resposta
         state = STATE_RESPONSE;
       } else {
         Serial.println("Connection failed!");
-        
-        // Desistimos da conexão e vamos para o estado de espera
         state = STATE_WAIT;
       }
       break;
     case STATE_RESPONSE:
-      if (client.available()) {
-        Serial.print(client.read());
-        // Continuar neste modo para obter mais caracteres de resposta
+      // verificar se há caracteres disponíveis na resposta do servidor
+      if(client.available()) {
+        // ler a reposta até não haver mais caracteres disponíveis
+        while(client.available()) {
+          char c = client.read();
+          Serial.print(c);
+        }
+      } else {
+        // tentar novamente até receber a resposta
+        // esta linha não é necessária devido a `state` já ser igual a STATE_RESPONSE,
+        // mas torna a intenção explícita
         state = STATE_RESPONSE;
-      } else if (!client.connected()) {
-        // Se o servidor desconectou, parar cliente e entrar em modo de espera
+      }
+      // Se o servidor desconectar (conforme pedimos pelo header Connection, do HTTP)
+      // paramos o cliente e vamos para o modod de espera, para pausar o arduino até o próximo envio de temperatura
+      if (!client.connected()) {
         Serial.println();
         Serial.println("disconnecting.");
         client.stop();
@@ -118,8 +140,10 @@ void loop() {
       }
       break;
     case STATE_WAIT:
+      Serial.println("Waiting for " + String(REQUEST_INTERVAL) + "ms");
       delay(REQUEST_INTERVAL);
+      // após a espera, voltamos para o modo de envio de temperatura
+      state = STATE_REQUEST;
       break;
   }
 }
-
