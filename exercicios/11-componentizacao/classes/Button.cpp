a94596b6fb113505024a0503381b03a2bbc88e05
Paolo Bueno <paolohaji@gmail.com>
2017-11-29 18:20:27 -0200

    Adicionar comentários

--- exercicios/11-componentizacao/classes/Button.cpp
+++ exercicios/11-componentizacao/classes/Button.cpp
@@ -1,29 +1,33 @@
+// O arquivo .cpp deve incluir seu próprio header
 #include "Button.h"
 
 /**
  * Construtor principal que recebe todos os parâmetros
  */
-Button::Button(unsigned int port, ASSINATURA_CALLBACK, unsigned int debounce) {
-	this->port = port;
+Button::Button(unsigned int pin, ASSINATURA_CALLBACK, unsigned int debounce) {
+	this->pin = pin;
 
 	// Delegamos a configuração dos outros parâmetros para funções específicas
 	setCallback(callback);
 	setDebounce(debounce);
 }
 
-Button::Button(unsigned int port, ASSINATURA_CALLBACK) {
-	this->port = port;
+Button::Button(unsigned int pin, ASSINATURA_CALLBACK) {
+	this->pin = pin;
 	setCallback(callback);
 	// Para as versões dos construtores que recebem menos parâmetros,
 	// só configuramos valores padrão
 	setDebounce(BUTTON_DEFAULT_DEBOUNCE);
 }
-Button::Button(unsigned int port) {
-	this->port = port;
+Button::Button(unsigned int pin) {
+	this->pin = pin;
 	setCallback(callback);
 	setDebounce(BUTTON_DEFAULT_DEBOUNCE);
 }
 
+/**
+ * Altera o pointer de callback interno para outra função
+ */
 void Button::setCallback(ASSINATURA_CALLBACK) {
 	this->callback = callback;
 }
@@ -36,22 +40,32 @@ void Button::setDebounce(unsigned int millis) {
 }
 
 void Button::loop() {
-	unsigned long now = millis();
-
-	if (now - this->lastHit < this->debounce) {
-		return;
-	}
-
+	// Caso não haja função de callback para chamar, não é necessário executar processamento
 	if (!callback) {
 		return;
 	}
 
-	int currentPinState = digitalRead(this->port);
+	// Tempo atual da execução
+	unsigned long now = millis();
 
+	// Se o tempo de debounce não se passou, fazer nada
+	if (now - this->lastHit < this->debounce) {
+		// A instrução return; numa função void (que não deve retornar valor)
+		// simplesmente finaliza a execução da função
+		return;
+	}
+
+	// Ler estado do botão
+	int currentPinState = digitalRead(this->pin);
+
+	// Caso o estado do botão tenha mudado...
 	if(!this->lastPinState && currentPinState) {
+		// ...armazenar o tempo do ultimo clique...
 		this->lastHit = now;
+		// ...e chamar a função de callback
 		this->callback();
 	}
 
+	// Armazenar estado anterior do botão
 	this->lastPinState = currentPinState;
 }
--- exercicios/11-componentizacao/classes/Button.h
+++ exercicios/11-componentizacao/classes/Button.h
@@ -4,14 +4,19 @@
 #ifndef BUTTON_h
 #define BUTTON_h
 
+// Dependências do módulo devem ser incluidas no arquivo .h
+// A biblioteca Arduino.h é a que é incluida por padrão pela IDE do Arduino, ela que contém as definições de `digitalWrite()`, `digitalRead()`, etc.
+// https://www.arduino.cc/reference/en/
 #include <Arduino.h>
 
 // Formato da assinatura do nosso callback,
 // um pointer para uma função que não recebe nenhum parâmetro
 #define ASSINATURA_CALLBACK void (*callback)()
 
+//
 #define BUTTON_DEFAULT_DEBOUNCE 200
 
+// Declaração de classe
 class Button {
 private:
 	unsigned int port;
@@ -20,11 +25,16 @@ private:
 	int lastPinState;
 	ASSINATURA_CALLBACK;
 public:
-	Button(unsigned int port);
-	Button(unsigned int port, ASSINATURA_CALLBACK);
-	Button(unsigned int port, ASSINATURA_CALLBACK, unsigned int debounce);
+	// Os métodos com o nome da própria classe são chamados Construtores
+	// cada classe necessita de pelo menos um, que tem a função de inicializar suas variáveis internas
+	Button(unsigned int pin);
+	Button(unsigned int pin, ASSINATURA_CALLBACK);
+	Button(unsigned int pin, ASSINATURA_CALLBACK, unsigned int debounce);
+
 	void setCallback(ASSINATURA_CALLBACK);
 	void setDebounce(unsigned int millis);
+
+	// A função loop() deve ser chamada em toda iteração do loop() do Arduino, é nela que o processamento principal da biblioteca
 	void loop();
 };
 
