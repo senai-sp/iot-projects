532273ab8bd7c334d492cecbffc3132256b493a1
Paolo Bueno <paolobueno@users.noreply.github.com>
2018-01-17 21:13:15 -0200

    Update j5-mqtt.js

--- exercicios/j5-mqtt.js
+++ exercicios/j5-mqtt.js
@@ -4,14 +4,49 @@ var board = new five.Board({
     port: "COM10"
 });
 
-var client = mqtt.connect("mqtt://test.mosquitto.org");
+board.on("ready", function () {
+    // a partir daqui, a board do johnny-five está pronta
+    console.log("Estou pronta pra ser usada...");
 
-client.on("connect", function () {
-    // a partir daqui, estamos conectados ao broker MQTT
-    board.on("ready", function () {
-        // a partir daqui, a board do johnny-five está pronta
-        console.log("Estou pronta pra ser usada...");
+    var button = new five.Button(8);
+    var portaoAberto = false;
+    var servo = new five.Servo({
+        pin: 10,
+        startAt: 19
+    });
+    function fecharPortao() {
+        servo.to(19, 1 * 1000);
+        portaoAberto = false;
+        console.log("Portão fechado");
+    }
+    function abrirPortao() {
+        servo.to(90, 1 * 1000);
+        portaoAberto = true;
+        console.log("Portão aberto");
+    }
 
+    button.on("down", function () {
+        if (portaoAberto) {
+            fecharPortao();
+        } else {
+            abrirPortao();
+        }
+    });
+
+    button.on("hold", function () {
+        console.log("hold");
+    });
+
+    button.on("up", function () {
+        console.log("up");
+    });
+
+    
+    var client = mqtt.connect("mqtt://localhost");
+
+    client.on("connect", function () {
+        console.log('MQTT Conectado!');
+        // a partir daqui, estamos conectados ao broker MQTT
         client.subscribe('codexp1-portao');
         function callback(topic, payload) {
             // recebemos "1" para abrir o portao
@@ -28,38 +63,5 @@ client.on("connect", function () {
             }
         }
         client.on("message", callback);
-
-        var button = new five.Button(8);
-        var portaoAberto = false;
-        var servo = new five.Servo({
-            pin: 10,
-            startAt: 19
-        });
-        function fecharPortao() {
-            servo.to(19, 1 * 1000);
-            portaoAberto = false;
-            console.log("Portão fechado");
-        }
-        function abrirPortao() {
-            servo.to(90, 1 * 1000);
-            portaoAberto = true;
-            console.log("Portão aberto");
-        }
-
-        button.on("down", function () {
-            if (portaoAberto) {
-                fecharPortao();
-            } else {
-                abrirPortao();
-            }
-        });
-
-        button.on("hold", function () {
-            console.log("hold");
-        });
-
-        button.on("up", function () {
-            console.log("up");
-        });
     });
 });
