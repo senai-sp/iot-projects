a5093cf196d1347d6838bd3b832099911c36cd24
Paolo Bueno <paolobueno@users.noreply.github.com>
2018-01-19 19:43:27 -0200

    Update j5-mqtt.js

--- exercicios/j5-mqtt.js
+++ exercicios/j5-mqtt.js
@@ -1,49 +1,18 @@
 var five = require("johnny-five");
 var mqtt = require("mqtt");
+var Portao = require("./Portao");
 var board = new five.Board({
     port: "COM10"
 });
+
+var config = require('./config');
+var broker = config.mqttBroker;
 
 board.on("ready", function () {
     // a partir daqui, a board do johnny-five está pronta
     console.log("Estou pronta pra ser usada...");
 
-    var button = new five.Button(8);
-    var portaoAberto = false;
-    var servo = new five.Servo({
-        pin: 10,
-        startAt: 19
-    });
-    function fecharPortao() {
-        servo.to(19, 1 * 1000);
-        portaoAberto = false;
-        console.log("Portão fechado");
-    }
-    function abrirPortao() {
-        servo.to(90, 1 * 1000);
-        portaoAberto = true;
-        console.log("Portão aberto");
-    }
-
-    button.on("down", function () {
-        if (portaoAberto) {
-            fecharPortao();
-        } else {
-            abrirPortao();
-        }
-    });
-
-    button.on("hold", function () {
-        console.log("hold");
-    });
-
-    button.on("up", function () {
-        console.log("up");
-    });
-
-    
-    var client = mqtt.connect("mqtt://localhost");
-
+    var client = mqtt.connect(broker);
     client.on("connect", function () {
         console.log('MQTT Conectado!');
         // a partir daqui, estamos conectados ao broker MQTT
@@ -54,14 +23,37 @@ board.on("ready", function () {
             var msg = payload.toString();
 
             if (msg === "1") {
-                abrirPortao();
+                portao.abrir();
             } else if (msg === "0") {
-                fecharPortao();
+                portao.fechar();
             } else {
                 console.log(topic);
                 console.log(msg);
             }
         }
         client.on("message", callback);
+    });
+
+    var button = new five.Button(8);
+    var servo = new five.Servo({
+        pin: 10,
+        startAt: 19
+    });
+    var servo2 = new five.Servo({
+        pin: 13,
+        startAt: 19
+    });
+    var portao = new Portao(servo);
+    var portao2 = new Portao(servo2);
+    button.on("down", function () {
+        portao.alternar();
+    });
+
+    button.on("hold", function () {
+        console.log("hold");
+    });
+
+    button.on("up", function () {
+        console.log("up");
     });
 });
